7a6b3e2cd915fdb2a66594e088a32c96473033ea
Mio Engel <mioengel@gmail.com>
2020-04-03 14:21:22 +0200

    Adds implementation

--- src/main/kotlin/se/vbgt/crypto/DiffieHellman.kt
+++ src/main/kotlin/se/vbgt/crypto/DiffieHellman.kt
@@ -3,8 +3,19 @@ package se.vbgt.crypto
 import java.math.BigInteger
 
 class DiffieHellman(val modulus: BigInteger, val base: BigInteger) {
-    fun setPrivateKey(privateKey: BigInteger): Nothing = TODO()
-    fun getPublicKey(): BigInteger = TODO()
-    fun setPartnersPublicKey(partnerPublicKey: BigInteger): Nothing = TODO()
-    fun getSecret(): BigInteger = TODO()
+    private lateinit var privateKey: BigInteger
+    private lateinit var secretKey: BigInteger
+    private val pKey: BigInteger by lazy { base.modPow(privateKey, modulus) }
+
+    fun setPrivateKey(privateKey: BigInteger) {
+        this.privateKey = privateKey
+    }
+
+    fun getPublicKey() = pKey
+
+    fun setPartnersPublicKey(partnerPublicKey: BigInteger) {
+        secretKey = partnerPublicKey.modPow(privateKey, modulus)
+    }
+
+    fun getSecret() = secretKey
 }
